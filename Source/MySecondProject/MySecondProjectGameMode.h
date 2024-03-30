29924c0eb4ccf8b68720814d0e7dd1facb2c4f26
Cherepashhka <valkunariks@gmail.com>
2024-03-30 23:52:30 +0200

    Fixes; added apples, cactus, death system, HUD

--- Source/MySecondProject/MySecondProjectGameMode.h
+++ Source/MySecondProject/MySecondProjectGameMode.h
@@ -3,11 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/GameModeBase.h"
+#include "GameFramework/GameMode.h"
 #include "MySecondProjectGameMode.generated.h"
 
 UCLASS(minimalapi)
-class AMySecondProjectGameMode : public AGameModeBase
+class AMySecondProjectGameMode : public AGameMode
 {
 	GENERATED_BODY()
 
